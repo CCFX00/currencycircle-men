28a4768cf76f156bed872d66c3869f9e80f5d842
Nya Steve <nyasteve29@gmail.com>
2024-04-08 18:00:21 +0100

    Updated uploadFile.js so that content is kept serialized in the CCFX Google drive

--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -14,17 +14,21 @@ const auth = new google.auth.GoogleAuth({
 exports.uploadFile = async (fileObject) => {
     const bufferStream = new stream.PassThrough();
     bufferStream.end(fileObject.buffer);
-    // console.log(bufferStream._readableState.buffer)
-    const { data } = await google.drive({ version: "v3", auth }).files.create({
-        media: {
-            mimeType: fileObject.mimeType,
-            body: bufferStream,
-        },
-        requestBody: {
-            name: fileObject.originalname,
-            parents: ["169bsCyktT3S0PiM3faY1gSD828EEIw1R"],
-        },
-        fields: "id,name",
-    });
-    // console.log(`Uploaded file ${data.name} ${data.id}`);
+    try{
+        const { data } = await google.drive({ version: "v3", auth }).files.create({
+            media: {
+                mimeType: 'application/octet-binary', // Setting the MIME type (application/octet-stream, application/octet-binary, application/x-binary)
+                body: bufferStream,
+            },
+            requestBody: {
+                name: `${fileObject.originalname}`,
+                parents: ["169bsCyktT3S0PiM3faY1gSD828EEIw1R"],
+            },
+            fields: "id,name",
+        });
+    }catch(error){
+        return{
+            message: `Error uploading file to Google Drive: ${error.message}`
+        }
+    }
 };
