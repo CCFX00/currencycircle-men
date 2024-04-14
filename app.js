220fe1b6ab0f7658fcfc0a6cd492d4cfe2bda28e
Nya Steve <nyasteve29@gmail.com>
2024-04-14 15:44:41 +0100

    installed packages express-session, passport, passport-google-oauth20, created files ssoAuth, ssoRoutes, oauthMidWare, auth.js, ssoControllers. Defined logic for OAuth with Google

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express')
 const app = express()
 const ErrorHandler = require('./middleware/error')
 const cookieParser = require('cookie-parser')
+// oAuth Google imports
+require('./utils/ssoAuth')
+const middlewareSetup = require('./middleware/oauthMidWareSetup')
 
 app.use(express.json())
 app.use(cookieParser())
+middlewareSetup(app)
 
 //importing routes
 const userRoutes = require('./routes/userRoutes')
+const ssoGoogleRoutes = require('./routes/ssoRoutes')
 
 app.use('/ccfx/api/v1', userRoutes)
+app.use('/ccfx/api/v1', ssoGoogleRoutes)
 
 app.use(ErrorHandler)
 
